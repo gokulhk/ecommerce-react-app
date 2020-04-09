ba107bb23fb49a3d0ebe8ee1ff9205b817e04203
gokulhk <gokulhk147@gmail.com>
2020-04-09 18:28:51 +0530

    initial setup files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+//React Router imports
+import { Switch, Route, Router } from 'react-router-dom'
+
+//React-Redux imports
+import { Provider } from 'react-redux';
+import mobileStore from './redux/mobiles/mobileStore';
+
+//In house Component imports
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail'
+import MyOrders from './components/MyOrders'
+import MyCart from './components/MyCart'
+import DefaultComp from './components/DefaultComp';
+
 function App() {
+
+  console.log("main com rendered")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={mobileStore}>
+
+      <div className="App">
+        <Switch>
+          <Route path='/' exact strict component={ProductList} />
+          <Route path='/details' exact strict component={ProductDetail} />
+          <Route path='/my_orders' exact strict component={MyOrders} />
+          <Route path='/my_cart' exact strict component={MyCart} />
+          <Route component={DefaultComp} />
+        </Switch>
+      </div>
+
+    </Provider>
   );
 }
 
