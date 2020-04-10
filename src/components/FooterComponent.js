bc499613dc1f4d066efc9df8c0efbcbde74b4b84
gokulhk <gokulhk147@gmail.com>
2020-04-10 22:03:54 +0530

    website footer

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FooterComponent() {
+    return (
+        <div className="navbar navbar-default navbar-fixed-bottom">
+            <div className="container-fluid">
+                <div className="navbar-text pull-left">
+                    <p>Copyright ShopNinja 2020</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FooterComponent
